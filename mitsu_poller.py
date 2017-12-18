6967cf49bc7cc14caf6aa1623be0e0757003f780
dbest <dbest@10.54.155.24>
2017-12-18 18:14:47 +1100

    Multi Units

--- mitsu_poller.py
+++ mitsu_poller.py
@@ -48,45 +48,49 @@ cool = 3
 fan = 7
 auto = 8
 
-# Uncomment to enable debug messages
-#mqttc.on_log = on_log
 
-url = urlparse(mqqt_url_str)
 
 #topic = url.path[1:] or '/sensors/hvac_downstairs/current_temp'
 
 # Get Mitshubisi Cookie
-auth_cookie = os.popen('curl --insecure -H \"Accept: application/json, text/javascript, */*\" -X POST -d \'{\"user\":\"%s\",\"pass\":\"%s\",\"appversion\":\"3.2.673a\"}\' %s/api/login.aspx -i -s | grep Set-Cookie | awk -F \'Set-Cookie: \' \'{print $2}\' | awk -F\';\' \'{print $1}\'' % ( user, password, melview_endpoint )).read().rstrip("\n")
+curl_cmd ='curl --insecure -H \"Accept: application/json, text/javascript, */*\" -X POST -d \'{\"user\":\"%s\",\"pass\":\"%s\",\"appversion\":\"3.2.673a\"}\' %s/api/login.aspx -i -s | grep Set-Cookie | awk -F \'Set-Cookie: \' \'{print $2}\' | awk -F\';\' \'{print $1}\'' % ( user, password, melview_endpoint )
+auth_cookie = os.popen(curl_cmd).read().rstrip("\n")
+
 # Get aircon units
-json_return = os.popen('curl --insecure -X GET -H \"Accept: application/json\" -H \"Cookie: %s\" %s/api/rooms.aspx?_=1513470690197 -s' % ( auth_cookie, melview_endpoint )).read().rstrip("\n")
+curl_cmd = 'curl --insecure -X GET -H \"Accept: application/json\" -H \"Cookie: %s\" %s/api/rooms.aspx?_=1513470690197 -s' % ( auth_cookie, melview_endpoint )
+json_return = os.popen(curl_cmd).read().rstrip("\n")
 unit_data = json.loads(json_return)
 
-#Get downstairs unit state
-unit_id = int(unit_data[0]["units"][1]["unitid"])
+# Count the number of airconditioners
+unit_count = len(unit_data[0]["units"])
 
-json_return = os.popen('curl --insecure -X GET -H \"Accept: application/json\" -H \"Cookie: %s\" -d \'{\"unitid\": %s, \"v\": 2}\' %s/api/unitcommand.aspx -s' % ( auth_cookie, unit_id, melview_endpoint )).read().rstrip("\n")
+#G Get the states for aeach unit
+unit_state = []
+for unit_num in range(0, unit_count, 1):
+    unit_id = int(unit_data[0]["units"][unit_num]["unitid"])
+    curl_cmd = 'curl --insecure -X GET -H \"Accept: application/json\" -H \"Cookie: %s\" -d \'{\"unitid\": %s, \"v\": 2}\' %s/api/unitcommand.aspx -s' % ( auth_cookie, unit_id, melview_endpoint )
+    json_return = os.popen(curl_cmd).read().rstrip("\n")
+    unit_state.append(json.loads(json_return))
 
-unit_state = json.loads(json_return)
-#pprint(unit_state)
-
-# Connect
+# Connect to MQTT, I'm using CloudMQTT but this will work using mosquito 
+# Uncomment to enable debug messages
+#mqttc.on_log = on_log
+url = urlparse(mqqt_url_str)
 mqttc.username_pw_set(url.username, url.password)
 mqttc.connect(url.hostname, url.port)
 
-# Start subscribe, with QoS level 0
-#mqttc.subscribe(topic, 0)
-
-# Publish a message
-mqttc.publish('/sensors/hvac_downstairs/current_temp', int(unit_data[0]["units"][1]["temp"]))
-mqttc.publish('/sensors/hvac_downstairs/temperature_state_topic', int(unit_data[0]["units"][1]["settemp"]))
-
-if unit_state["setmode"] == auto:
-  mqttc.publish('/sensors/hvac_downstairs/mode_state_topic', 'auto')
-elif unit_state["setmode"] == cool:
-  mqttc.publish('/sensors/hvac_downstairs/mode_state_topic', 'cool')
-elif unit_state["setmode"] == heat:
-  mqttc.publish('/sensors/hvac_downstairs/mode_state_topic', 'heat')
-elif unit_state["setmode"] == dry:
-  mqttc.publish('/sensors/hvac_downstairs/mode_state_topic', 'dry')
-elif unit_state["setmode"] == fan_only:
-  mqttc.publish('/sensors/hvac_downstairs/mode_state_topic', 'fan_only')
+for unit_num in range(0, unit_count, 1):
+    unit_name = unit_data[0]["units"][unit_num]["room"].lower()
+# Publish stes for each unit to MQTT
+    mqttc.publish('/sensors_hvac/%s/current_temp' % unit_name, int(unit_data[0]["units"][unit_num]["temp"]))
+    mqttc.publish('/sensors_hvac/%s/temperature_state_topic' % unit_name, int(unit_data[0]["units"][unit_num]["settemp"]))
+    if unit_state[unit_num]["setmode"] == auto:
+      mqttc.publish('/sensors_hvac/%s/mode_state_topic' % unit_name, 'auto')
+    elif unit_state[unit_num]["setmode"] == cool:
+      mqttc.publish('/sensors_hvac/%s/mode_state_topic' % unit_name, 'cool')
+    elif unit_state[unit_num]["setmode"] == heat:
+      mqttc.publish('/sensors_hvac/%s/mode_state_topic' % unit_name, 'heat')
+    elif unit_state[unit_num]["setmode"] == dry:
+      mqttc.publish('/sensors_hvac/%s/mode_state_topic' % unit_name, 'dry')
+    elif unit_state[unit_num]["setmode"] == fan_only:
+      mqttc.publish('/sensors_hvac/%s/mode_state_topic' % unit_name, 'fan_only')
