f92e9d75c3b643356751c6a7ea68573e6a1d5b21
Delwin <mail@delwinbest.com>
2017-12-19 21:25:54 +1100

    Optimized loops and moved to requests

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ This is a MQTT poller for the Mitsubishi range of WIFI controllers. Tested using
 ## Requirements:
 pip3 install paho-mqtt
 pip3 install configparser
+pip3 install requests
 
 ## Usage:
 TBC, rough notes:
@@ -19,10 +20,40 @@ climate:
     mode_command_topic: /sensors_hvac/downstairs/mode_command_topic
     mode_state_topic: /sensors_hvac/downstairs/mode_state_topic
   - platform: mqtt
-    name: Lounge
-    current_temperature_topic: /sensors_hvac/lounge/current_temp
-    temperature_command_topic: /sensors_hvac/lounge/target_temp
-    temperature_state_topic: /sensors_hvac/lounge/temperature_state_topic
-    power_command_topic: /sensors_hvac/lounge/power_command_topic
-    mode_command_topic: /sensors_hvac/lounge/mode_command_topic
-    mode_state_topic: /sensors_hvac/lounge/mode_state_topic
+    name: downstairs
+    current_temperature_topic: /sensors_hvac/downstairs/current_temp
+    temperature_command_topic: /sensors_hvac/downstairs/target_temp
+    temperature_state_topic: /sensors_hvac/downstairs/temperature_state_topic
+    power_command_topic: /sensors_hvac/downstairs/power_command_topic
+    mode_command_topic: /sensors_hvac/downstairs/mode_command_topic
+    mode_state_topic: /sensors_hvac/downstairs/mode_state_topic
+    
+    
+    
+    
+## Melview API return format
+POST /api/unitcommand.aspx
+{	"unitid":"116903",
+	"v":2,
+	"lc":1,
+	"commands":"AV5"
+}
+REPLY:
+{	"id":"116903",
+	"lc":"",
+	"power":1,
+	"standby":0,
+	"setmode":8,
+	"automode":3,
+	"setfan":0,
+	"settemp":"23",
+	"roomtemp":"24",
+	"outdoortemp":"30",
+	"airdir":5,
+	"airdirh":3,
+	"sendcount":0,
+	"fault":"",
+	"error":"ok"
+}
+
+
--- mitsu_poller.py
+++ mitsu_poller.py
@@ -5,7 +5,7 @@ import os,json
 from urllib.parse import urlparse
 from pprint import pprint
 import configparser 
-
+import requests
 
 
 # Define event callbacks
@@ -54,18 +54,21 @@ melview_endpoint = "https://api.melview.net"
 # Aircon Modes
 # See what I did here :)
 aircon_modes = ['0','heat','dry','cool','4','5','6','fan_only','auto']
-
+aircon_fanspeeds = ['0','1','2','3','4','5']
 
 
 # Get Mitshubisi Cookie
-curl_cmd ='curl --insecure -H \"Accept: application/json, text/javascript, */*\" -X POST -d \'{\"user\":\"%s\",\"pass\":\"%s\",\"appversion\":\"3.2.673a\"}\' %s/api/login.aspx -i -s | grep Set-Cookie | awk -F \'Set-Cookie: \' \'{print $2}\' | awk -F\';\' \'{print $1}\'' % ( user, password, melview_endpoint )
-#print(curl_cmd)
-auth_cookie = os.popen(curl_cmd).read().rstrip("\n")
+headers = {'Accept': 'application/json, text/javascript, */*'}
+payload = {'user': user, 'pass': password, 'appversion': '3.2.673a'}
+r = requests.post("%s/api/login.aspx" % melview_endpoint, headers=headers, data=json.dumps(payload))
+if r.status_code != 200:
+    print("ERROR: Melview endpoint HTTP error code %s" % r.status_code )
 
 # Get aircon units
-curl_cmd = 'curl --insecure -X GET -H \"Accept: application/json\" -H \"Cookie: %s\" %s/api/rooms.aspx?_=1513470690197 -s' % ( auth_cookie, melview_endpoint )
-json_return = os.popen(curl_cmd).read().rstrip("\n")
-json_format = json.loads(json_return)
+auth_cookie = r.headers.get('Set-cookie')
+headers = {'Accept': 'application/json, text/javascript, */*', 'Cookie': auth_cookie }
+r = requests.post("%s/api/rooms.aspx?_=1513470690197" % melview_endpoint, headers=headers)
+json_format = json.loads(r.text)
 unit_data = json_format[0]
 
 # Count the number of airconditioners
@@ -75,8 +78,9 @@ unit_count = len(unit_data["units"])
 unit_state = []
 for unit_num in range(0, unit_count, 1):
     unit_id = int(unit_data["units"][unit_num]["unitid"])
-    curl_cmd = 'curl --insecure -X GET -H \"Accept: application/json\" -H \"Cookie: %s\" -d \'{\"unitid\": %s, \"v\": 2}\' %s/api/unitcommand.aspx -s' % ( auth_cookie, unit_id, melview_endpoint )
-    json_return = os.popen(curl_cmd).read().rstrip("\n")
+    payload = {'unitid': unit_id, 'v': '2'}
+    r = requests.post("%s/api/unitcommand.aspx" % melview_endpoint, headers=headers, data=json.dumps(payload))
+    json_return = r.text
     unit_state.append(json.loads(json_return))
 
 # Connect to MQTT, I'm using CloudMQTT but this will work using mosquito 
@@ -89,15 +93,21 @@ mqttc.connect(url.hostname, url.port)
 # Subscript to the HVAC topic
 #mqttc.subscribe([("/sensors_hvac/lounge/mode_command_topic", 0),("/sensors_hvac/downstairs/mode_command_topic", 0),("/sensors_hvac/lounge/target_temp", 0),("/sensors_hvac/downstairs/target_temp", 0)])
 
+# Iterate through the json list and publish each key/value pair to a topic.
 for unit_num in range(0, unit_count, 1):
     unit_name = unit_data["units"][unit_num]["room"].lower()
 # Publish states for each unit to MQTT
-    mqttc.publish('/sensors_hvac/%s/current_temp' % unit_name, int(unit_data["units"][unit_num]["temp"]))
-    mqttc.publish('/sensors_hvac/%s/temperature_state_topic' % unit_name, int(unit_data["units"][unit_num]["settemp"]))
-    mqttc.publish('/sensors_hvac/%s/mode_state_topic' % unit_name, aircon_modes[unit_state[unit_num]["setmode"]])
+    for key, value in unit_data["units"][unit_num].items():
+        print(key, value)
+        if key == 'mode':
+            value = aircon_modes[unit_state[unit_num]["setmode"]]
+        if key == 'mode':
+            value = aircon_modes[unit_state[unit_num]["setmode"]]
+        mqttc.publish('/sensors_hvac/%s/%s' % (unit_name, key), value)
+#    mqttc.publish('/sensors_hvac/%s/temperature_state_topic' % unit_name, int(unit_data["units"][unit_num]["settemp"]))
+#    mqttc.publish('/sensors_hvac/%s/mode_state_topic' % unit_name, aircon_modes[unit_state[unit_num]["setmode"]])
 
       
 #mqttc.loop_forever()      
-      
-      
-mqttc.disconnect()      
+mqttc.disconnect() 
+   
